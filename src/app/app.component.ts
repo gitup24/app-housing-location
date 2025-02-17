dbde210b17885780edf238b69b031d1bb5c5227e
m e <duchatelle@gmail.com>
2025-02-17 02:40:54 +0100

    routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 //5:39
 import {HomeComponent} from "./home/home.component";
+//5 3:8
+import { RouterModule} from "@angular/router";
 
 @Component({
   standalone: true,
@@ -14,12 +16,13 @@ import {HomeComponent} from "./home/home.component";
       </header>
       <!--4:40 child cpnt. Import suggéré-->
       <section class="content">
-        <app-home></app-home>
+        <router-outlet></router-outlet>
+        <!--<app-home></app-home>-->
       </section>
     </main>
   `,
   styleUrls: ['./app.component.css'],
-  imports: [HomeComponent]
+  imports: [HomeComponent, RouterModule]
 })
 export class AppComponent {
   title = 'homes';
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent)
-  .catch(err => console.error(err));
+import { provideRouter } from "@angular/router";
+//5 5:45
+import routeConfig from "./app/routes";
+//5 1:04
+bootstrapApplication(AppComponent, {
+    providers: [
+        //5 6:04 provideRouter([])
+        provideRouter(routeConfig)
+    ]
+}).catch(err => console.error(err));
